593bfef45edb1db8c4ea12d249b941a1dd5b4e8c
Tue Christensen <tue@qampo.com>
2024-07-24 11:28:04 +0200

    Made a more precise typehint for the generate_instance function

--- tspdashboard/utilities.py
+++ tspdashboard/utilities.py
@@ -1,11 +1,10 @@
 """Various utility functions for the TSP Dashboard application."""
 
-from typing import Any
 
 import numpy as np
 
 
-def generate_instance(no_cities: int = 10) -> Any:
+def generate_instance(no_cities: int = 10) -> np.ndarray:
     """Generates a new instance of the TSP problem with size."""
     gen = np.random.default_rng()
     return gen.random((no_cities, 2))
